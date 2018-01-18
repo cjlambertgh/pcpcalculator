ef3e7b2bf2b210d18ba4baa16336aa999800659f
chris <phateuk@gmail.com>
2018-01-18 17:45:40 +0000

    Add form to index page

--- pcpcalculator/Pages/Index.cshtml.cs
+++ pcpcalculator/Pages/Index.cshtml.cs
@@ -19,24 +19,40 @@ namespace pcpcalculator.Pages
         }
         public void OnGet()
         {
-            var financeData = new FinanceData
-            {
-                Price = 10000,
-                Rate = 6,
-                DealerContribution = 0,
-                FinalPayment = 5000,
-                Deposit = 0,
-                Term = 36
-            };
-            var payments = financeCalculatorService.LoanRepayments(financeData);
-            var pcpPayments = financeCalculatorService.PcpLoanRepayments(financeData);
+            //var financeData = new FinanceData
+            //{
+            //    Price = 10000,
+            //    Rate = 6,
+            //    DealerContribution = 0,
+            //    FinalPayment = 5000,
+            //    Deposit = 0,
+            //    Term = 36
+            //};
+            //var payments = financeCalculatorService.LoanRepayments(financeData);
+            //var pcpPayments = financeCalculatorService.PcpLoanRepayments(financeData);
         }
 
+        [BindProperty]
         public FinanceData FinanceData { get; set; }
 
-        //public async Task<IActionResult> OnPostAsync()
-        //{
+        public LoanRepaymentDetails LoanRepaymentDetails { get; set; }
 
-        //}
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            LoanRepaymentDetails = financeCalculatorService.PcpLoanRepayments(FinanceData);
+            return Page();
+        }
+
+    }
+
+    public class FinanceResult
+    {
+        FinanceData FinanceData { get; set; }
+
     }
 }
